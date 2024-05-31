64faebe0d7ca4fc4e8ac70d62e75f60ede6ea51f
RolandBistrischi <bistrischiroland@gmail.com>
2024-05-31 18:29:42 +0300

    chestii cu php

--- Client-Server/Client-Server/WebService.asmx.cs
+++ Client-Server/Client-Server/WebService.asmx.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Timers;
+using System.Web;
 using System.Web.Services;
 
 namespace Client_Server
@@ -17,6 +20,17 @@ namespace Client_Server
     public class WebService : System.Web.Services.WebService
     {
         private readonly string connectionString = "Data Source=DESKTOP-6ICN792;Initial Catalog = BancaDatabase; Integrated Security = True";
+        private Timer timer;
+
+        public WebService()
+        {
+            // Inițializează și configurează timer-ul
+            timer = new Timer
+            {
+                Interval = 12 * 60 * 60 * 1000 // Intervalul în milisecunde (12 ore)
+            };
+            timer.Elapsed += TimerElapsed;
+        }
 
         [WebMethod]
         public bool AdugareUser( Utilizator utilizator )
@@ -296,15 +310,228 @@ namespace Client_Server
             return valute;
         }
 
+        [WebMethod]
+        public int GetUserIdbyIban( string iban )
+        {
+            if (string.IsNullOrWhiteSpace(iban.Trim()))
+                return -1;
 
+            string query = "SELECT id_user FROM Useri WHERE iban = @IBAN";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
 
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@IBAN", iban.Trim());
+                        int id = Convert.ToInt32(command.ExecuteScalar());
 
+                        return id;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("A apărut o excepție: " + ex.Message);
+                return -1;
+            }
+        }
 
+        [WebMethod]
+        public Utilizator GetUtilizatorByIban( string iban )
+        {
+            if (string.IsNullOrWhiteSpace(iban.Trim()))
+                return null;
 
+            string query = "SELECT * FROM Useri WHERE iban = @IBAN";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
 
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@IBAN", iban.Trim());
 
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                Utilizator utilizator = new Utilizator
+                                {
+                                    IdUser = Convert.ToInt32(reader ["id"]),
+                                    Sold = reader ["sold"] != DBNull.Value ? Convert.ToDecimal(reader ["sold"]) : 0,
+                                    Iban = iban,
+                                    Nume = reader ["nume"].ToString(),
+                                    Prenume = reader ["prenume"].ToString(),
+                                    Cnp = reader ["cnp"].ToString(),
+                                    Telefon = reader ["telefon"].ToString(),
+                                    DataCreare = Convert.ToDateTime(reader ["data_creare"]),
+                                    IdValuta = Convert.ToInt32(reader ["id_valuta"]),
+                                };
 
+                                return utilizator;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("A apărut o excepție: " + ex.Message);
+                return null;
+            }
+            return null;
+        }
 
+        [WebMethod]
+        public bool Tranzactie( Utilizator utilizator, string iban, decimal pret )
+        {
+            Utilizator utilizator_destinatie = GetUtilizatorByIban(iban);
+
+            if (utilizator == null || utilizator_destinatie == null)
+                return false;
+
+            if (utilizator.IdUser <= 0 || utilizator_destinatie.IdUser <= 0)
+                return false;
+
+            if (utilizator.Sold < pret || pret <= 0)
+                return false;
+
+            try
+            {
+
+                // Obține rata de schimb valutar între valuta utilizatorului sursă și valuta utilizatorului destinatar
+                decimal rataDeSchimb = GetExchangeRate(utilizator.IdValuta, utilizator_destinatie.IdValuta);
+
+                // Calculează echivalentul sumei trimise în valuta utilizatorului destinatar
+                decimal sumaInValutaDestinatar = pret * rataDeSchimb;
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlTransaction transaction = connection.BeginTransaction())
+                    {
+                        try
+                        {
+                            string insertQuery = "INSERT INTO tranzactii (Id_user_sursa, Id_user_destinatie, Id_valuta, suma, data_tranzactie, stare_tranzactie) VALUES (@id_user_sursa, @id_user_destinatie, @id_valuta, @suma, GETDATE(), 'In curs')";
+                            using (SqlCommand command = new SqlCommand(insertQuery, connection, transaction))
+                            {
+                                command.Parameters.AddWithValue("@id_user_sursa", utilizator.IdUser);
+                                command.Parameters.AddWithValue("@id_user_destinatie", utilizator_destinatie.IdUser);
+                                command.Parameters.AddWithValue("@id_valuta", utilizator_destinatie.IdValuta);
+                                command.Parameters.AddWithValue("@suma", sumaInValutaDestinatar);
+                                command.ExecuteNonQuery();
+                            }
+
+                            // Actualizează soldul utilizatorului sursă
+                            decimal newSoldSursa = utilizator.Sold - pret;
+                            string updateQuerySursa = "UPDATE Useri SET sold = @sold WHERE id = @id";
+                            using (SqlCommand command = new SqlCommand(updateQuerySursa, connection, transaction))
+                            {
+                                command.Parameters.AddWithValue("@sold", newSoldSursa);
+                                command.Parameters.AddWithValue("@id", utilizator.IdUser);
+                                command.ExecuteNonQuery();
+                            }
+
+                            // Actualizează soldul utilizatorului destinatar
+                            string updateQueryDestinatie = "UPDATE Useri SET sold = sold + @suma WHERE id = @id";
+                            using (SqlCommand command = new SqlCommand(updateQueryDestinatie, connection, transaction))
+                            {
+                                command.Parameters.AddWithValue("@suma", sumaInValutaDestinatar);
+                                command.Parameters.AddWithValue("@id", utilizator_destinatie.IdUser);
+                                command.ExecuteNonQuery();
+                            }
+
+                            transaction.Commit();
+                            return true;
+                        }
+                        catch (Exception)
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return false;
+        }
+
+        private decimal GetExchangeRate( int idValuta1, int idValuta2 )
+        {
+            throw new NotImplementedException();
+        }
+
+        [WebMethod]
+        public string ExecutePhp()
+        {
+            try
+            {
+                // Calea relativă la directorul aplicației pentru executabilul PHP
+                string phpPath = HttpContext.Current.Server.MapPath("~/cursbnr/php.exe");
+
+                // Calea relativă la directorul aplicației pentru fișierul PHP care trebuie executat
+                string scriptPath = HttpContext.Current.Server.MapPath("~/cursbnr/CursBNR.php");
+
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = phpPath,
+                    Arguments = scriptPath,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+
+                    process.WaitForExit();
+
+                    if (process.ExitCode == 0)
+                    {
+                        return output;
+                    }
+                    else
+                    {
+                        return $"Error: {error}";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Exception: {ex.Message}";
+            }
+        }
+
+        private void TimerElapsed( object sender, ElapsedEventArgs e )
+        {
+            ExecutePhp(); // Apelarea funcției ExecutePhp la intervale de 12 ore
+        }
+
+        [WebMethod]
+        public void StartTimer()
+        {
+            timer.Start();
+        }
+
+        [WebMethod]
+        public void StopTimer()
+        {
+            timer.Stop();
+        }
 
         private string GetConnectionString()
         {
---
+++ Client-Server/Client-Server/cursbnr/CursBNR.php
@@ -0,0 +1,115 @@
+<?php
+	/*
+	 * Class CursBNR v1.02
+	 * This class parses BNR's XML and returns the current exchange rate
+	 *
+	 * Requirements: PHP5 
+	 *
+	 * Last update: October 2011, 27	 
+	 * More info: https://www.curs-valutar-bnr.ro
+	 */
+
+	 $host = 'localhost';
+$db = 'BancaDatabase';
+$user = 'utilizator';
+$pass = 'parola';
+	
+ 	class CursBNR
+ 	{
+ 		/**
+		 * xml document
+		 * @var string
+		 */
+ 		var $xmlDocument = "";
+ 		
+ 		
+ 		/**
+		 * exchange date
+		 * BNR date format is Y-m-d
+		 * @var string
+		 */
+ 		var $date = "";
+ 		
+ 		
+ 		/**
+		 * currency
+		 * @var associative array
+		 */
+ 		var $currency = array();
+ 		
+ 		
+ 		/**
+		 * cursBnrXML class constructor
+		 *
+		 * @access		public
+		 * @param 		$url		string
+		 * @return		void
+		 */
+		function __construct($url)
+		{
+			$this->xmlDocument = file_get_contents($url);
+			$this->parseXMLDocument();
+		}
+ 		
+		/**
+		 * parseXMLDocument method
+		 *
+		 * @access		public
+		 * @return 		void
+		 */
+		function parseXMLDocument()
+		{
+	 		$xml = new SimpleXMLElement($this->xmlDocument);
+	 		
+	 		$this->date=$xml->Header->PublishingDate;
+	 		
+	 		foreach($xml->Body->Cube->Rate as $line)	
+	 		{ 		 			
+	 			$this->currency[]=array("name"=>$line["currency"], "value"=>$line, "multiplier"=>$line["multiplier"]);
+	 		}
+		}
+		
+		/**
+		 * getCurs method
+		 * 
+		 * get current exchange rate: example getExchangeRate("USD")
+		 * 
+		 * @access		public
+		 * @return 		double
+		 */
+		function getExchangeRate($currency)
+		{
+			foreach($this->currency as $line)
+			{
+				if($line["name"]==$currency)
+				{
+					return $line["value"];
+				}
+			}
+			
+			return "Incorrect currency!";
+		}
+ 	}
+ 	
+ 	//-----------------------------------------------------------------------------------------------------------------------------
+    //@an example of using the CursBNR class
+     
+     $curs=new CursBNR("https://www.bnr.ro/nbrfxrates.xml");
+     
+    // Interogarea pentru a obține lista de valute din baza de date
+$stmt = $pdo->query("SELECT currency_code FROM valute");
+$currencies = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+// Actualizarea cursurilor valutare în baza de date
+foreach($currencies as $currency) {
+    $rate = $curs->getExchangeRate($currency);
+    if (is_numeric($rate)) {
+        $stmt = $pdo->prepare("UPDATE valute SET rate = :rate WHERE currency_code = :currency");
+        $stmt->execute(['rate' => $rate, 'currency' => $currency]);
+    } else {
+        echo "Currency $currency not found or incorrect!\n";
+    }
+}
+
+echo "Cursurile valutare au fost actualizate cu succes!";
+?>
